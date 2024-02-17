f6c7178d7a88cd83b9c43aa8febab27103315bfd
flucium <flucium@flucium.net>
2024-02-17 01:30:14 +0000

    update ./crates/flat-ast/src/statement.rs
    added redirect and redirect operator.

--- crates/flat-ast/src/statement.rs
+++ crates/flat-ast/src/statement.rs
@@ -17,3 +17,16 @@ pub struct Assign {
     pub ident: Expr,
     pub expr: Expr,
 }
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Redirect{
+    pub left: Expr,
+    pub right: Expr,
+    pub operator: RecirectOperator,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum RecirectOperator{
+    Gt,
+    Lt,
+}

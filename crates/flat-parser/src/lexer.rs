58fcaba954685f02021b7b6dba8cb10874f8c480
flucium <flucium@flucium.net>
2024-02-19 06:35:20 +0000

    refactor ./crates/flat-parser/src/lexer.rs
    tests

--- crates/flat-parser/src/lexer.rs
+++ crates/flat-parser/src/lexer.rs
@@ -3,7 +3,7 @@ use crate::token::Token;
 pub struct Lexer {
     input: Vec<char>,
     position: usize,
-    is_eof:bool,
+    is_eof: bool,
 }
 
 impl Lexer {
@@ -82,12 +82,11 @@ impl Lexer {
                     let origin = self.position;
 
                     if let Some(peek_ch) = self.input.get(self.position + 1) {
-                        
                         if peek_ch.is_whitespace() == false {
                             self.position += 1;
 
                             let string = self.read_string();
-                            
+
                             if let Ok(n) = string.parse::<i32>() {
                                 tkn = Token::FD(n);
                             } else {
@@ -122,7 +121,7 @@ impl Lexer {
 
         tkn
     }
-    
+
     fn read_string(&mut self) -> String {
         let is_double_quote = self.input[self.position] == '"';
 
@@ -165,9 +164,9 @@ impl Iterator for Lexer {
         let tkn = self.read();
 
         if tkn == Token::EOF {
-            if self.is_eof{
+            if self.is_eof {
                 None
-            }else{
+            } else {
                 self.is_eof = true;
                 Some(tkn)
             }
@@ -181,28 +180,20 @@ impl Iterator for Lexer {
 mod tests {
     use super::*;
 
+    /*
+        Test lexer with pseudo command
+    */
+
     #[test]
-    fn test_lexer() {
-        let input = r#"
-            "hello" "world" @1 @2 3
-            | ; = > < & $
-        "#;
+    fn test_lexer_pseudo_command() {
+        let input = "ls -a ./";
 
         let mut lexer = Lexer::new(input);
 
         let expected = vec![
-            Token::String("hello".to_string()),
-            Token::String("world".to_string()),
-            Token::FD(1),
-            Token::FD(2),
-            Token::USize(3),
-            Token::Pipe,
-            Token::Semicolon,
-            Token::Assign,
-            Token::Gt,
-            Token::Lt,
-            Token::Ampersand,
-            Token::Dollar,
+            Token::String("ls".to_string()),
+            Token::String("-a".to_string()),
+            Token::String("./".to_string()),
             Token::EOF,
         ];
 
@@ -211,8 +202,294 @@ mod tests {
         }
     }
 
+    /*
+        Test lexer with semicolon
+        - test_lexer_semicolon
+        - test_lexer_semicolon_with_space
+    */
+
+    #[test]
+    fn test_lexer_semicolon() {
+        let input = ";";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+    }
+
+    #[test]
+    fn test_lexer_semicolon_with_space() {
+        let input = " ; ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Semicolon);
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    /*
+        Test lexer with pipe
+        - test_lexer_pipe
+        - test_lexer_pipe_with_space
+    */
+
+    #[test]
+    fn test_lexer_pipe() {
+        let input = "|";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Pipe);
+    }
+
+    #[test]
+    fn test_lexer_pipe_with_space() {
+        let input = " | ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Pipe);
+    }
+
+    /*
+        Test lexer with assign
+        - test_lexer_assign
+        - test_lexer_assign_with_space
+    */
+
+    #[test]
+    fn test_lexer_assign() {
+        let input = "=";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Assign);
+    }
+
+    #[test]
+    fn test_lexer_assign_with_space() {
+        let input = " = ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Assign);
+    }
+
+    /*
+        Test lexer with gt
+        - test_lexer_gt
+        - test_lexer_gt_with_space
+    */
+
+    #[test]
+    fn test_lexer_gt() {
+        let input = ">";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Gt);
+    }
+
+    #[test]
+    fn test_lexer_gt_with_space() {
+        let input = " > ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Gt);
+    }
+
+    /*
+        Test lexer with lt
+        - test_lexer_lt
+        - test_lexer_lt_with_space
+    */
+
+    #[test]
+    fn test_lexer_lt() {
+        let input = "<";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Lt);
+    }
+
+    #[test]
+    fn test_lexer_lt_with_space() {
+        let input = " < ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Lt);
+    }
+
+    /*
+        Test lexer with ampersand
+        - test_lexer_ampersand
+        - test_lexer_ampersand_with_space
+    */
+
+    #[test]
+    fn test_lexer_ampersand() {
+        let input = "&";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Ampersand);
+    }
+
+    #[test]
+    fn test_lexer_ampersand_with_space() {
+        let input = " & ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Ampersand);
+    }
+
+    /*
+        Test lexer with dollar
+        - test_lexer_dollar
+        - test_lexer_dollar_with_space
+    */
+
+    #[test]
+    fn test_lexer_dollar() {
+        let input = "$";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Dollar);
+    }
+
+    #[test]
+    fn test_lexer_dollar_with_space() {
+        let input = " $ ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Dollar);
+    }
+
+    /*
+        Test lexer with fd
+        - test_lexer_fd
+        - test_lexer_fd_with_space
+    */
+
+    #[test]
+    fn test_lexer_fd() {
+        let input = "@123";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::FD(123));
+    }
+
+    #[test]
+    fn test_lexer_fd_with_space() {
+        let input = " @123 ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::FD(123));
+    }
+
+    /*
+        Test lexer with usize
+        - test_lexer_usize
+        - test_lexer_usize_with_space
+    */
+
+    #[test]
+    fn test_lexer_usize() {
+        let input = "123";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::USize(123));
+    }
+
+    #[test]
+    fn test_lexer_usize_with_space() {
+        let input = " 123 ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::USize(123));
+    }
+
+    /*
+        Test lexer with eof
+        - test_lexer_eof
+        - test_lexer_eof_with_space
+        - test_lexer_some(eof)_none
+    */
+
+    #[test]
+    fn test_lexer_eof() {
+        let input = "";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn test_lexer_eof_with_space() {
+        let input = " ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn test_lexer_some_eof_none() {
+        let input = " ";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+
+        assert_eq!(lexer.next(), None);
+    }
+
+    /*
+
+        Test lexer with string
+        - test_lexer_string
+        - test_lexer_string_with_space
+        - test_lexer_string_double_quote
+    */
+
     #[test]
     fn test_lexer_string() {
+        let input = "abcd";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::String("abcd".to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn test_lexer_string_with_space() {
+        let input = "abcd efgh";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::String("abcd".to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::String("efgh".to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::EOF);
+    }
+
+    #[test]
+    fn test_lexer_string_double_quote() {
         let input = r#"
             "hello" "world" "hello world"
         "#;
@@ -231,41 +508,64 @@ mod tests {
         }
     }
 
+    /*
+        Test lexer with ident
+        - test_lexer_ident
+        - test_lexer_ident_with_space
+        - test_lexer_ident_double_quote
+    */
 
     #[test]
-    fn test_lexer_ident(){
+    fn test_lexer_ident() {
+        let input = "$hello";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Ident("hello".to_string()));
+    }
+
+    #[test]
+    fn test_lexer_ident_with_space() {
+        let input = "$hello world";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next().unwrap(), Token::Ident("hello".to_string()));
+
+        assert_eq!(lexer.next().unwrap(), Token::String("world".to_string()));
+    }
+
+    #[test]
+    fn test_lexer_ident_double_quote() {
+        let input = "$\"hello world\"";
+
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(
+            lexer.next().unwrap(),
+            Token::Ident("hello world".to_string())
+        );
+    }
+
+    /*
+        Test lexer with mixed
+        - test_lexer_mixed
+    */
+
+    #[test]
+    fn test_lexer_mixed() {
         let input = r#"
-            $hello $world
+            $hello "world" 123 456 @789
         "#;
 
         let mut lexer = Lexer::new(input);
 
         let expected = vec![
             Token::Ident("hello".to_string()),
-            Token::Ident("world".to_string()),
-            Token::EOF,
-        ];
-
-        for tkn in expected {
-            assert_eq!(lexer.next(), Some(tkn));
-        }
-    }
-
-
-    #[test]
-    fn test_lexer_string_with_pipe() {
-        let input = r#"
-            "hello" | "world" | "hello world"
-        "#;
-
-        let mut lexer = Lexer::new(input);
-
-        let expected = vec![
-            Token::String("hello".to_string()),
-            Token::Pipe,
             Token::String("world".to_string()),
-            Token::Pipe,
-            Token::String("hello world".to_string()),
+            Token::USize(123),
+            Token::USize(456),
+            Token::FD(789),
             Token::EOF,
         ];
 
@@ -273,67 +573,4 @@ mod tests {
             assert_eq!(lexer.next(), Some(tkn));
         }
     }
-
-    #[test]
-    fn test_lexer_string_with_semicolon() {
-        let input = r#"
-            "hello" ; "world" ; "hello world"
-        "#;
-
-        let mut lexer = Lexer::new(input);
-
-        let expected = vec![
-            Token::String("hello".to_string()),
-            Token::Semicolon,
-            Token::String("world".to_string()),
-            Token::Semicolon,
-            Token::String("hello world".to_string()),
-            Token::EOF,
-        ];
-
-        for tkn in expected {
-            assert_eq!(lexer.next(), Some(tkn));
-        }
-    }
-
-    #[test]
-    fn test_lexer_usize(){
-        let input = r#"
-            1 2 3
-        "#;
-
-        let mut lexer = Lexer::new(input);
-
-        let expected = vec![
-            Token::USize(1),
-            Token::USize(2),
-            Token::USize(3),
-            Token::EOF,
-        ];
-
-        for tkn in expected {
-            assert_eq!(lexer.next(), Some(tkn));
-        }
-    }
-
-    #[test]
-    fn test_lexer_fd(){
-        let input = r#"
-            @1 @2 @3
-        "#;
-
-        let mut lexer = Lexer::new(input);
-
-        let expected = vec![
-            Token::FD(1),
-            Token::FD(2),
-            Token::FD(3),
-            Token::EOF,
-        ];
-
-        for tkn in expected {
-            assert_eq!(lexer.next(), Some(tkn));
-        }
-    }
-
 }

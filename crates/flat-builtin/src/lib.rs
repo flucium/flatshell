bb6569bf8fad844a4826f76b87b083f385d9c911
flucium <flucium@flucium.net>
2024-02-22 18:15:27 +0000

    add ./crates/flat-builtin/src/lib.rs

--- crates/flat-builtin/src/lib.rs
+++ crates/flat-builtin/src/lib.rs
@@ -1,4 +1,16 @@
-// cd
-// ls
-// abort
-// exit
+mod common;
+
+#[cfg(target_os = "linux")]
+mod linux;
+
+#[cfg(target_os = "macos")]
+mod darwin;
+
+#[cfg(target_os = "linux")]
+pub use linux::*;
+
+
+#[cfg(target_os = "macos")]
+pub use darwin::*;
+
+pub use common::*;

42f25f99d6b93fffa368bb8935e722a2ebae0e8f
flucium <flucium@flucium.net>
2024-03-07 05:07:29 +0000

    update ./crates/flat-ast/src/lib.rs

--- crates/flat-ast/src/lib.rs
+++ crates/flat-ast/src/lib.rs
@@ -2,16 +2,17 @@ mod statement;
 mod expr;
 mod pipe;
 
+use serde::Serialize;
+
 //pub use
 pub use statement::*;
 pub use expr::*;
 pub use pipe::*;
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq,Serialize)]
 pub enum FlatAst {
     Semicolon(Vec<FlatAst>),
-    Pipe(Vec<FlatAst>),
-    Expr(Expr),
+    Pipe(Pipe),
     Statement(Statement),
 }
 
@@ -19,5 +20,12 @@ impl FlatAst {
     pub fn new() -> Self {
         FlatAst::Semicolon(Vec::new())
     }
-    
+
+    pub fn to_json(&self, is_pretty: bool) -> String {
+        if is_pretty {
+            serde_json::to_string_pretty(&self).unwrap()
+        } else {
+            serde_json::to_string(&self).unwrap()
+        }
+    }    
 }

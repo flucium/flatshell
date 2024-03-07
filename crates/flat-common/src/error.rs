17fa50a968c957e680ce478198cba282bacdc9d8
flucium <flucium@flucium.net>
2024-03-07 03:31:31 +0000

    update ./crates/flat-common/src/error.rs
    pub struct Error message:&'static str -> String

--- crates/flat-common/src/error.rs
+++ crates/flat-common/src/error.rs
@@ -7,27 +7,36 @@ pub enum ErrorKind {
     NotFound,
     PermissionDenied,
 
-    Syntax,
+    SyntaxError,
 }
 
 /// Error
 #[derive(Debug)]
 pub struct Error {
     kind: ErrorKind,
-    message: &'static str,
+    // message: &'static str,
+    message: String,
 }
 
 impl Error {
     /// Dummy error
     pub const DUMMY: Self = Self {
         kind: ErrorKind::Dummy,
-        message: "Dummy error",
+        message: String::new(),
     };
 
     /// Create new error
-    pub fn new(kind: ErrorKind, message: &'static str) -> Self {
+    pub fn new(kind: ErrorKind, message: &str) -> Self {
+        let message = message.to_string();
         Self { kind, message }
     }
+
+    // pub fn new_with_string(kind: ErrorKind, message: String) -> Self {
+    //     Self {
+    //         kind,
+    //         message: Box::leak(message.into_boxed_str()),
+    //     }
+    // }
 
     /// Get error kind
     pub fn kind(&self) -> &ErrorKind {
@@ -35,7 +44,7 @@ impl Error {
     }
 
     /// Get error message
-    pub fn message(&self) -> &'static str {
-        self.message
+    pub fn message(&self) -> &str {
+        &self.message
     }
 }

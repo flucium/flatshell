e8e56a5c72a9e4e82630edaa20b3d0c8eb6a887f
flucium <flucium@flucium.net>
2024-03-13 01:40:55 +0000

    update ./crates/flat-terminal/src/ascii.rs
    added unit test.

--- crates/flat-terminal/src/ascii.rs
+++ crates/flat-terminal/src/ascii.rs
@@ -1,5 +1,5 @@
 
-pub enum Cursor {
+pub(super) enum Cursor {
     Move(usize),
     Backspace,
     Left,
@@ -8,7 +8,7 @@ pub enum Cursor {
 }
 
 impl Cursor {
-    pub fn get_esc_code(&self) -> String {
+    pub(super) fn get_esc_code(&self) -> String {
         return match &self {
             Cursor::Move(position) => format!("\x1b[{position}G"),
             Cursor::Backspace => format!("\x08{}", " "),
@@ -16,5 +16,28 @@ impl Cursor {
             Cursor::Right => format!("\x1b[1C"),
             Cursor::ClearLine => format!("\x1b[2K"),
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cursor_get_esc_code() {
+        let cursor = Cursor::Move(5);
+        assert_eq!(cursor.get_esc_code(), "\x1b[5G");
+
+        let cursor = Cursor::Backspace;
+        assert_eq!(cursor.get_esc_code(), "\x08 ");
+
+        let cursor = Cursor::Left;
+        assert_eq!(cursor.get_esc_code(), "\x1b[1D");
+
+        let cursor = Cursor::Right;
+        assert_eq!(cursor.get_esc_code(), "\x1b[1C");
+
+        let cursor = Cursor::ClearLine;
+        assert_eq!(cursor.get_esc_code(), "\x1b[2K");
     }
 }

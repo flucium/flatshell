ca152e214f7a82c15abc58ffea11da32eef87ec5
flucium <flucium@flucium.net>
2024-04-02 11:30:15 +0000

    clean ./app/src/main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,19 +1,10 @@
-fn main() {
-    // REP LOOP. Demo.
-    let mut terminal = flat_terminal::Terminal::new();
+fn main() {}
+/*
+    let ast = flat_parser::Parser::new(flat_parser::Lexer::new("$A = ping;$A -c 3 flucium.net;ls ./ -a|cat -b"))
+        .parse()
+        .unwrap();
 
-    let mut state = flat_engine::State::new();
+    let  state = &mut flat_engine::State::new();
 
-    // ping -c 3 127.0.0.1 | cat -b
-    // $A = ping; $A -c 3 127.0.0.1 | cat -b
-    terminal.set_prompt("> ");
-    loop {
-        let input = terminal.read_line().unwrap();
-
-        let ast = flat_parser::Parser::new(flat_parser::Lexer::new(&input))
-            .parse()
-            .unwrap();
-
-        flat_engine::eval(ast, &mut state).unwrap();
-    }
-}
+    flat_engine::eval(ast, state).unwrap();
+*/

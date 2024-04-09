4625338508dbbf418240f9d05d56f45de53cfb9d
flucium <flucium@flucium.net>
2024-04-09 06:14:05 +0000

    refactor ./crates/flat-engine/src/lib.rs

--- crates/flat-engine/src/lib.rs
+++ crates/flat-engine/src/lib.rs
@@ -1,6 +1,7 @@
 mod pipe;
 mod process_handler;
 mod sh_vars;
+mod state;
 use flat_common::{
     error::{Error, ErrorKind},
     result::Result,
@@ -8,7 +9,7 @@ use flat_common::{
 pub use pipe::*;
 pub use process_handler::*;
 pub use sh_vars::*;
-
+pub use state::*;
 use std::{
     os::unix::io::{AsRawFd, FromRawFd},
     process,
@@ -17,97 +18,6 @@ use std::{
 /*
     ToDo: Redirect, Builtin Command
 */
-
-#[derive(Debug)]
-pub struct State {
-    vars: ShVars,
-    handler: ProcessHandler,
-    pipe: Pipe,
-}
-
-impl State {
-    pub fn new() -> Self {
-        Self {
-            vars: ShVars::new(),
-            handler: ProcessHandler::new(),
-            pipe: Pipe::new(),
-        }
-    }
-
-    pub fn vars(&mut self) -> &mut ShVars {
-        &mut self.vars
-    }
-}
-
-impl From<ShVars> for State {
-    fn from(vars: ShVars) -> Self {
-        Self {
-            vars,
-            handler: ProcessHandler::new(),
-            pipe: Pipe::new(),
-        }
-    }
-}
-
-impl From<ProcessHandler> for State {
-    fn from(handler: ProcessHandler) -> Self {
-        Self {
-            vars: ShVars::new(),
-            handler,
-            pipe: Pipe::new(),
-        }
-    }
-}
-
-impl From<Pipe> for State {
-    fn from(pipe: Pipe) -> Self {
-        Self {
-            vars: ShVars::new(),
-            handler: ProcessHandler::new(),
-            pipe,
-        }
-    }
-}
-
-impl From<(ShVars, ProcessHandler)> for State {
-    fn from((vars, handler): (ShVars, ProcessHandler)) -> Self {
-        Self {
-            vars,
-            handler,
-            pipe: Pipe::new(),
-        }
-    }
-}
-
-impl From<(ShVars, Pipe)> for State {
-    fn from((vars, pipe): (ShVars, Pipe)) -> Self {
-        Self {
-            vars,
-            handler: ProcessHandler::new(),
-            pipe,
-        }
-    }
-}
-
-impl From<(ProcessHandler, Pipe)> for State {
-    fn from((handler, pipe): (ProcessHandler, Pipe)) -> Self {
-        Self {
-            vars: ShVars::new(),
-            handler,
-            pipe,
-        }
-    }
-}
-
-impl From<(ShVars, ProcessHandler, Pipe)> for State {
-    fn from((vars, handler, pipe): (ShVars, ProcessHandler, Pipe)) -> Self {
-        Self {
-            vars,
-            handler,
-            pipe,
-        }
-    }
-}
 
 pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
     match ast.to_owned() {
@@ -122,7 +32,9 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
             while let Some(command) = pipe.pop_front() {
                 // let is_redirect = command.redirects.is_empty();
 
-                let mut ps_command = create_process_command(command, state);
+                let is_background = command.background;
+
+                let mut ps_command = create_process_command(command, state)?;
 
                 // if pipe.is_empty() && is_redirect{
                 //     ps_command.stdout(process::Stdio::inherit());
@@ -133,14 +45,20 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
                 }
 
                 let pid = state.handler.push(
-                    ps_command
-                        .spawn()
-                        .map_err(|_| Error::new(ErrorKind::NotFound, ""))?,
+                    ps_command.spawn().map_err(|err| {
+                        if err.kind() == std::io::ErrorKind::NotFound {
+                            Error::new(ErrorKind::NotFound, "Command not found")
+                        } else {
+                            Error::new(ErrorKind::EngineError, "Command execution failed")
+                        }
+                    })?,
+                    is_background,
                 );
 
                 if let Some(child) = state.handler.get(pid) {
                     if let Some(stdout) = child.stdout.as_ref() {
-                        state.pipe.send(stdout.as_raw_fd());
+                        let fd = stdout.as_raw_fd();
+                        state.pipe.send(fd);
                     }
                 }
             }
@@ -152,11 +70,20 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
 
         flat_ast::FlatAst::Statement(statement) => match statement.to_owned() {
             flat_ast::Statement::Command(command) => {
-                let mut ps_command = create_process_command(command, state);
+                let is_background = command.background;
 
-                state
-                    .handler
-                    .push(ps_command.spawn().expect("Failed to spawn process"));
+                let mut ps_command = create_process_command(command, state)?;
+
+                state.handler.push(
+                    ps_command.spawn().map_err(|err| {
+                        if err.kind() == std::io::ErrorKind::NotFound {
+                            Error::new(ErrorKind::NotFound, "Command not found")
+                        } else {
+                            Error::new(ErrorKind::EngineError, "Command execution failed")
+                        }
+                    })?,
+                    is_background,
+                );
 
                 state.handler.wait();
             }
@@ -164,7 +91,10 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
             flat_ast::Statement::Assign(assign) => {
                 let ident = match assign.ident {
                     flat_ast::Expr::Ident(ident) => ident,
-                    _ => Err(Error::DUMMY)?,
+                    _ => Err(Error::new(
+                        ErrorKind::EngineError,
+                        "Command execution failed. Invalid abstract syntax tree",
+                    ))?,
                 };
 
                 let value = match assign.expr {
@@ -172,7 +102,10 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
 
                     flat_ast::Expr::USize(number) => number.to_string(),
 
-                    _ => Err(Error::DUMMY)?,
+                    _ => Err(Error::new(
+                        ErrorKind::EngineError,
+                        "Command execution failed. Invalid abstract syntax tree",
+                    ))?,
                 };
 
                 state.vars.insert(&ident, &value);
@@ -183,13 +116,56 @@ pub fn eval(ast: flat_ast::FlatAst, state: &mut State) -> Result<()> {
     Ok(())
 }
 
-fn create_process_command(command: flat_ast::Command, state: &mut State) -> process::Command {
+fn create_process_command(
+    command: flat_ast::Command,
+    state: &mut State,
+) -> Result<process::Command> {
     // name
-    let program = get_command_name(command.expr, state);
+    let program = get_command_name(command.expr, state)?;
 
     // args
-    let args = get_command_args(command.args, state);
+    let args = get_command_args(command.args, state)?;
 
+    // stdin, stdout, stderr
+    let (stdin, stdout, stderr) = set_command_stdio(state);
+
+    let mut ps_command = process::Command::new(program);
+
+    ps_command.args(args);
+
+    ps_command.stdin(stdin);
+
+    ps_command.stdout(stdout);
+
+    ps_command.stderr(stderr);
+
+    Ok(ps_command)
+}
+
+// Set command stdin, stdout, stderr with redirect
+//
+// If a pipe is attached to Stdin, Stdout, or Stderr, and any of them overlaps with Redirect, the pipe will be discarded.
+//
+// specifically, it closes the pipe in the State structure.
+//
+// fn set_command_stdio_redirect(
+//     redirects: Vec<flat_ast::Redirect>,
+//     stdin: process::Stdio,
+//     stdout: process::Stdio,
+//     stderr: process::Stdio,
+//     state: &mut State,
+// ) -> (process::Stdio, process::Stdio, process::Stdio) {
+//     if redirects.is_empty() {
+//         return (stdin, stdout, stderr);
+//     }
+
+//     redirects.iter().for_each(|redirect| todo!());
+
+//     todo!()
+// }
+
+/// Set command stdin, stdout, stderr
+fn set_command_stdio(state: &mut State) -> (process::Stdio, process::Stdio, process::Stdio) {
     // stdin
     let stdin = if state.pipe.is_recvable() {
         unsafe { process::Stdio::from_raw_fd(state.pipe.recv()) }
@@ -207,59 +183,11 @@ fn create_process_command(command: flat_ast::Command, state: &mut State) -> proc
     // stderr
     let stderr = process::Stdio::inherit();
 
-    // redirect
-    // command.redirects.iter().for_each(|redirect| {
-    //     let left = match redirect.left {
-    //         flat_ast::Expr::FD(fd) => fd,
-    //         _ => {
-    //             todo!()
-    //         }
-    //     };
-
-    //     let right = match redirect.right.to_owned() {
-    //         flat_ast::Expr::String(string) => fs::File::options().create(true).write(true).read(true).open(string).unwrap(),
-    //         flat_ast::Expr::FD(fd) => unsafe { fs::File::from_raw_fd(fd) },
-    //         _ => {
-    //             todo!()
-    //         }
-    //     };
-
-    //     if !matches!(left, 0 | 1 | 2) { /*dup2 */ }
-
-    //     match redirect.operator {
-    //         flat_ast::RedirectOperator::Gt => {
-    //             if left == 1 {
-    //                 stdout = process::Stdio::from(right);
-    //             } else if left == 2 {
-    //                 stderr = process::Stdio::from(right);
-    //             } else {
-    //                 // error
-    //             }
-    //         }
-    //         flat_ast::RedirectOperator::Lt => {
-    //             if left == 0 {
-    //                 stdin = process::Stdio::from(right);
-    //             } else {
-    //                 // error
-    //             }
-    //         }
-    //     }
-    // });
-
-    let mut ps_command = process::Command::new(program);
-
-    ps_command.args(args);
-
-    ps_command.stdin(stdin);
-
-    ps_command.stdout(stdout);
-
-    ps_command.stderr(stderr);
-
-    ps_command
+    (stdin, stdout, stderr)
 }
 
-fn get_command_args(args: Vec<flat_ast::Expr>, state: &mut State) -> Vec<String> {
+/// Get command arguments
+fn get_command_args(args: Vec<flat_ast::Expr>, state: &mut State) -> Result<Vec<String>> {
     let mut v = Vec::with_capacity(args.len());
 
     args.iter().for_each(|arg| match arg.to_owned() {
@@ -272,26 +200,32 @@ fn get_command_args(args: Vec<flat_ast::Expr>, state: &mut State) -> Vec<String>
                 .to_string(),
         ),
         flat_ast::Expr::USize(number) => v.push(number.to_string()),
-        _ => {
-            todo!()
-        }
+        _ => Err(Error::new(
+            ErrorKind::EngineError,
+            "Command execution failed. Invalid abstract syntax tree",
+        ))
+        .unwrap(),
     });
 
-    v
+    Ok(v)
 }
 
-fn get_command_name(expr: flat_ast::Expr, state: &mut State) -> String {
+/// Get command name
+fn get_command_name(expr: flat_ast::Expr, state: &mut State) -> Result<String> {
     match expr {
-        flat_ast::Expr::String(string) => string,
+        flat_ast::Expr::String(string) => Ok(string),
 
-        flat_ast::Expr::Ident(ident) => state
+        flat_ast::Expr::Ident(ident) => Ok(state
             .vars
             .get(&ident)
             .unwrap_or(&String::default())
-            .to_string(),
+            .to_string()),
 
-        flat_ast::Expr::USize(number) => number.to_string(),
+        flat_ast::Expr::USize(number) => Ok(number.to_string()),
 
-        _ => todo!(),
+        _ => Err(Error::new(
+            ErrorKind::EngineError,
+            "Command execution failed. Invalid abstract syntax tree",
+        )),
     }
 }

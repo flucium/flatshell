e34d55919fe9a2973a79a700eae2cfc1ef097024
flucium <flucium@flucium.net>
2024-04-12 06:29:42 +0000

    update ./crates/flat-builtin/src/*

--- crates/flat-builtin/src/common.rs
+++ crates/flat-builtin/src/common.rs
@@ -1,4 +1,7 @@
-use std::process;
+use std::{
+    io::{stdout, Write},
+    process,
+};
 
 pub fn exit(code: i32) {
     process::exit(code)
@@ -6,4 +9,25 @@ pub fn exit(code: i32) {
 
 pub fn abort() {
     process::abort()
+}
+
+pub fn printenv(key: &str, vars:Vec<(&String, &String)>) {
+    let mut stdout = stdout().lock();
+
+    if key.len() == 0 {
+        for (key, value) in vars.iter() {
+            stdout
+                .write_fmt(format_args!("{}={}\n", key, value))
+                .unwrap();
+        }
+
+        return;
+    }
+
+    for (k, v) in vars.iter() {
+        if k == &key {
+            stdout.write_fmt(format_args!("{}={}\n", k, v)).unwrap();
+            return;
+        }
+    }
 }
--- crates/flat-builtin/src/lib.rs
+++ crates/flat-builtin/src/lib.rs
@@ -1,32 +1,11 @@
-mod common;
+pub mod common;
 
 #[cfg(any(target_os = "linux", target_os = "macos", target_os = "unix"))]
-mod unix;
+pub mod unix;
 
 pub fn is_builtin(command: &str) -> bool {
     match command {
         "abort" | "cd" | "exit" => true,
         _ => false,
-    }
-}
-
-pub fn execute(command: &str, args: Vec<&str>) {
-    match command {
-        "abort" => {
-            common::abort();
-        }
-
-        "cd" => {
-            #[cfg(any(target_os = "linux", target_os = "macos", target_os = "unix"))]
-            unix::cd(args.get(0).unwrap_or(&"./"));
-        }
-
-        "exit" => {
-            let arg = args.get(0).unwrap_or(&"0").parse::<i32>().unwrap_or(2);
-
-            common::exit(arg);
-        }
-
-        _ => {}
     }
 }

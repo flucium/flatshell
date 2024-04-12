56ab66db3dd87dd43e77ea4cc8dc5a47f2a8043f
flucium <flucium@flucium.net>
2024-04-12 04:20:13 +0000

    temporary commit

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,1 +1,36 @@
-fn main(){}
+use flat_engine::{eval, State};
+use flat_parser::Parser;
+use flat_terminal::Terminal;
+
+/*
+    ToDo: Error handling
+*/
+
+/*
+    PROFILE: $HOME/.fsh_profile
+
+    DEBUG PROFILE: ./temp/.fsh_profile
+*/
+
+/*
+    $FSH_PROMPT = "fsh> "
+    $FSH_HISTORY = ".fsh_history"
+    $FSH_HISTORY_SIZE = 1000
+    $FSH_HISTORY_ENABLED = true
+*/
+
+fn main() {
+    let mut state = State::new();
+
+    let mut terminal = Terminal::new();
+
+    loop {
+        terminal.set_prompt(flat_terminal::prompt("\\W> "));
+
+        let string = terminal.read_line().unwrap();
+
+        let ast = Parser::new(&string).parse().unwrap();
+
+        eval(ast, &mut state).unwrap();
+    }
+}

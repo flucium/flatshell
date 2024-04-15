5d822a5ee15b7db99c7cdc660e6acbc3b634e6ec
flucium <flucium@flucium.net>
2024-04-15 14:40:33 +0900

    update ./crates/fsh-engine/src/state.rs

--- crates/fsh-engine/src/state.rs
+++ crates/fsh-engine/src/state.rs
@@ -1,28 +1,23 @@
-use super::{pipe::*, process_handler::*, sh_vars::*};
+use std::path::{Path, PathBuf};
+
+use fsh_common::{Error, Result};
+
+use super::{pipe::*, process_handler::*};
 
 #[derive(Debug)]
 pub struct State {
-    vars: ShVars,
     handler: ProcessHandler,
     pipe: Pipe,
+    current_dir: PathBuf,
 }
 
 impl State {
-    /// Create a new state
     pub fn new() -> Self {
         Self {
-            vars: ShVars::new(),
             handler: ProcessHandler::new(),
             pipe: Pipe::new(),
+            current_dir: PathBuf::new(),
         }
-    }
-
-    pub fn vars(&self) -> &ShVars {
-        &self.vars
-    }
-
-    pub fn vars_mut(&mut self) -> &mut ShVars {
-        &mut self.vars
     }
 
     pub fn handler(&self) -> &ProcessHandler {
@@ -39,5 +34,44 @@ impl State {
 
     pub fn pipe_mut(&mut self) -> &mut Pipe {
         &mut self.pipe
+    }
+
+    pub fn current_dir(&self) -> Result<&Path> {
+        if self.current_dir.exists() == false {
+            Err(Error::new(
+                fsh_common::ErrorKind::NotFound,
+                "no such file or directory",
+            ))?
+        }
+
+        if self.current_dir.is_dir() == false {
+            Err(Error::new(
+                fsh_common::ErrorKind::NotADirectory,
+                "not a directory",
+            ))?
+        }
+
+        Ok(&self.current_dir)
+    }
+
+    pub fn set_current_dir(&mut self, path: &Path)->Result<()> {
+        
+        if path.exists() == false{
+            Err(Error::new(
+                fsh_common::ErrorKind::NotFound,
+                "no such file or directory",
+            ))?
+        }
+
+        if path.is_dir() == false{
+            Err(Error::new(
+                fsh_common::ErrorKind::NotADirectory,
+                "not a directory",
+            ))?
+        }
+
+        self.current_dir = path.to_path_buf();
+        
+        Ok(())
     }
 }
